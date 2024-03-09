9676ff6efca8d6c3cf0a3569e3c01b721ee8910c
Ff-c109 <xjy20060326xjy@outlook.com>
2024-03-09 21:10:07 +0800

    [v1.0.2] 增加了Markdown支持的语法

--- NativeC/markdown_decode.cpp
+++ NativeC/markdown_decode.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <sstream>
 
+using namespace std;
+
 namespace markdown_decode {
      std::string markdownToHTML(std::string markdown) {
         for(int i = 0; i < markdown.length() ; i++) {
@@ -16,16 +18,80 @@ namespace markdown_decode {
         std::string line;
         std::string html = "";
 
+        string buffer = "";
+        bool bufferEmpty = true;
+
         while (std::getline(mdStream, line)) {
+            if(!bufferEmpty) {
+                if (line[0] == '=') {
+                    bool isLine = true;
+                    for(const auto c : line) {
+                        if(c != '=')
+                            if(c != '\n' && c != ' ')
+                                isLine = false;
+                            else
+                                break;
+                    }
+                    if(isLine) {
+                        html += "<h1>";
+                        html += buffer;
+                        html += "</h1>\n";
+                        buffer = "";
+                        bufferEmpty = true;
+                    }
+                    continue;
+                }
+                if (line[0] == '-') {
+                    bool isLine = true;
+                    for(const auto c : line) {
+                        if(c != '-')
+                            if(c != '\n' && c != ' ')
+                                isLine = false;
+                            else
+                                break;
+                    }
+                    if(isLine) {
+                        html += "<h2>";
+                        html += buffer;
+                        html += "</h2>\n";
+                        buffer = "";
+                        bufferEmpty = true;
+                    }
+                    continue;
+                }
+                else {
+                    html += "<p>";
+                    html += buffer;
+                    html += "</p>\n";
+                    buffer = "";
+                    bufferEmpty = true;
+                }
+            }
+
             // 标题语法
             if (line[0] == '#') {
                 int count = 0;
                 while (line[count] == '#') { count++; }
                 html += "<h" + std::to_string(count) + ">" + line.substr(count) + "</h" + std::to_string(count) + ">\n";
             }
+            else if (line[0] == '-') {
+                bool isLine = true;
+                for(const auto c : line) {
+                    if(c != '-')
+                        if(c != '\n' && c != ' ')
+                            isLine = false;
+                        else
+                            break;
+                }
+                if(isLine) {
+                    html += "<hr/>";
+                    html += "\n";
+                }
+            }
             // 段落语法
             else {
-                html += "<p>" + line + "</p>\n";
+                buffer += line;
+                bufferEmpty = false;
             }
         }
 
